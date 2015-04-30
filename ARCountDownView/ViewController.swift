71961de76ac4f9e9ba6005d07d2490b7f2976d55
nero-tang <nerotang@Yufeis-MacBook-Pro.local>
2015-04-30 19:08:48 -0400

    Initial Commit

---
+++ ARCountDownView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARCountDownView
+//
+//  Created by Yufei Tang on 2015-04-30.
+//  Copyright (c) 2015 Yufei Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
